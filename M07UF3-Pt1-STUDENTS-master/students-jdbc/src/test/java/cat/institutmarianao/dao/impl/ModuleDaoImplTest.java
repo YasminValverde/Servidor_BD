bcc792113c685dcd7956c866053585f5c0560fa8
YasminValverde <yasminvalverde5@gmail.com>
2026-04-08 17:00:35 +0200

    Modules hechos el dao

--- M07UF3-Pt1-STUDENTS-master/students-jdbc/src/test/java/cat/institutmarianao/dao/impl/ModuleDaoImplTest.java
+++ M07UF3-Pt1-STUDENTS-master/students-jdbc/src/test/java/cat/institutmarianao/dao/impl/ModuleDaoImplTest.java
@@ -1,7 +1,59 @@
 package cat.institutmarianao.dao.impl;
 
-public class ModuleDaoImplTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-	// TODO ModuleDaoImpl tests
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import cat.institutmarianao.dao.ModuleDao;
+import cat.institutmarianao.model.Module;
+import jakarta.ejb.EJB;
+
+
+public class ModuleDaoImplTest extends BaseDaoImpl<Module, String> implements ModuleDao {
+
+	@EJB
+	private ModuleDao moduleDao;
+
+	@Override
+	public Module findByPk(String moduleCode, String cycleCode)
+			throws ClassNotFoundException, SQLException, IOException {
+		Module module = moduleDao.findByPk(moduleCode, cycleCode);
+		assertNotNull(module);
+		assertEquals("M01", module.getCode());
+		assertEquals("DAM", module.getCycleCode());
+		assertEquals("Sistemes informàtics", module.getName());
+		return module;
+	}
+
+
+
+	@Override
+	public List<Module> findAllByCycleCode(String cycleCode) throws ClassNotFoundException, SQLException, IOException {
+		List<Module> modules = moduleDao.findAllByCycleCode(cycleCode);
+		assertNotNull(modules);
+		assertEquals(2, modules.size());
+		return modules;
+	}
+
+
+
+	@Override
+	protected Module buildObjectFromResultSet(ResultSet rs) throws SQLException {
+		Module module = new Module();
+		module.setCode(rs.getString("M01"));
+		module.setCycleCode(rs.getString("DAM"));
+		module.setName(rs.getString("Sistemes informàtics"));
+		return module;
+	}
 }
--- M07UF3-Pt1-STUDENTS-master/students-jdbc/src/test/java/cat/institutmarianao/dao/impl/StudentDaoImplTest.java
+++ M07UF3-Pt1-STUDENTS-master/students-jdbc/src/test/java/cat/institutmarianao/dao/impl/StudentDaoImplTest.java
@@ -1,6 +1,60 @@
 package cat.institutmarianao.dao.impl;
 
-public class StudentDaoImplTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-	// TODO StudentDaoImpt tests
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import cat.institutmarianao.dao.ModuleDao;
+import cat.institutmarianao.dao.StudentDao;
+import cat.institutmarianao.model.Module;
+import cat.institutmarianao.model.Student;
+import jakarta.ejb.EJB;
+
+public class StudentDaoImplTest extends BaseDaoImpl<Student	, String> implements StudentDao {
+	@EJB
+	private StudentDao StudentDao;
+	
+	@Override
+	public Student findByPk(String dni) throws ClassNotFoundException, SQLException, IOException {
+		throws ClassNotFoundException, SQLException, IOException {
+			Student student = StudentDao.findByPk(dni);
+			assertNotNull(student);
+			assertEquals(dni,student.getDni());
+			return student;
+	}
+
+	@Override
+	public List<Student> findAll() throws ClassNotFoundException, SQLException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student add(Student student) throws ClassNotFoundException, SQLException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(Student student) throws ClassNotFoundException, SQLException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeByDni(String dni) throws ClassNotFoundException, SQLException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected Student buildObjectFromResultSet(ResultSet rs) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
